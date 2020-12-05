8731e164a2321a409b2886f45de79ed298b6d180
Anton-L <70330052+Anton-L-GitHub@users.noreply.github.com>
2020-12-05 16:44:49 +0100

    sql

---
+++ SQL/uppgift/proc.sql
@@ -0,0 +1,20 @@
+--- Proc 1
+EXEC Educations
+
+--- Proc 2
+EXEC Education_search @Education = 'DevOps Engineer', @Location = 'Solna'
+
+--- Proc 3
+
+--- Proc 4
+
+--- Proc 5
+
+--- Proc 6
+
+--- Proc 7
+
+--- Proc 8
+
+
+
